78127f227f99887a0f8be582b5626c6e9754b8aa
Elvis Diaz <elvisdiaz020@gmail.com>
2023-02-14 18:31:52 -0500

    adds current stable version of quiz game function

--- script.js
+++ script.js
@@ -35,8 +35,6 @@ function generateList() {
   answers.appendChild(list);
 }
 
-function playQuiz() {}
-
 let question1 = {
   question: "What is the full name of HTML?",
   choice1: "Hyper Type Making Languague",
@@ -82,13 +80,14 @@ function sendMessage() {
 
 function startQuiz() {
   setTime();
-  game.textContent = question1.question;
-  answer1.textContent = question1.choice1;
-  answer2.textContent = question1.choice2;
-  answer3.textContent = question1.choice3;
-  answer4.textContent = question1.choice4;
-  playQuiz();
+  //   game.textContent = question1.question;
+  //   answer1.textContent = question1.choice1;
+  //   answer2.textContent = question1.choice2;
+  //   answer3.textContent = question1.choice3;
+  //   answer4.textContent = question1.choice4;
+  //   playQuiz();
   //   generateList();
+  playGame();
 }
 
 button.addEventListener("click", startQuiz);
@@ -108,22 +107,70 @@ answer1.addEventListener("click", function (event) {
   console.log(user);
 });
 
-let questionContainer = document.querySelector("#question1");
-questionContainer.addEventListener("click", function (event) {
-  let element = event.target;
+function playGame() {
+  let questionContainer = document.querySelector("#question1");
+  questionContainer.addEventListener("click", function (event) {
+    let element = event.target;
 
-  if (element.matches("#question1")) {
-    // var state = element.getAttribute("data-state");
-    var answerChoice = element.getAttribute("data-answer");
-
-    if (answerChoice === "correct-answer") {
-      //   element.setAttribute("data-state", "visible");
-      //   element.textContent = text;
-      console.log("correct");
-    } else {
-      //   element.setAttribute("data-state", "hidden");
-      //   element.textContent = "";
-      console.log("incorrect");
+    if (element.matches(".choice")) {
+      // var state = element.getAttribute("data-state");
+      var answerChoice = element.getAttribute("data-answer");
+      let status = element.getAttribute("data-status");
+      if (answerChoice === "correct-answer") {
+        element.setAttribute("data-status", "clicked");
+        if (status === "clicked") {
+          console.log("clicked");
+        }
+      } else {
+        //   element.setAttribute("data-state", "hidden");
+        //   element.textContent = "";
+        console.log("incorrect");
+      }
     }
-  }
-});
+  });
+
+  let questionContainer2 = document.querySelector("#question2");
+  questionContainer2.addEventListener("click", function (event) {
+    let element = event.target;
+
+    if (element.matches(".choice2")) {
+      // var state = element.getAttribute("data-state");
+      var answerChoice2 = element.getAttribute("data-answer");
+      let status = element.getAttribute("data-status");
+      if (answerChoice2 === "correct-answer") {
+        element.setAttribute("data-status", "clicked");
+        if (status === "clicked") {
+          console.log("clicked");
+        }
+      } else {
+        //   element.setAttribute("data-state", "hidden");
+        //   element.textContent = "";
+        console.log("incorrect");
+      }
+    }
+  });
+
+  let questionContainer3 = document.querySelector("#question3");
+  questionContainer3.addEventListener("click", function (event) {
+    let element = event.target;
+
+    if (element.matches(".choice3")) {
+      // var state = element.getAttribute("data-state");
+      var answerChoice3 = element.getAttribute("data-answer");
+      let status = element.getAttribute("data-status");
+      if (answerChoice3 === "correct-answer") {
+        element.setAttribute("data-status", "clicked");
+        if (status === "clicked") {
+          console.log("clicked");
+          game.textContent = "game done";
+        }
+        //   element.textContent = text;
+        console.log("correct");
+      } else {
+        //   element.setAttribute("data-state", "hidden");
+        //   element.textContent = "";
+        console.log("incorrect");
+      }
+    }
+  });
+}
